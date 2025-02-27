a1feadcc296d9eb8bccfc54e822e375a2875ac8e
anthony tony <anthony.ellison120@yahoo.com>
2025-02-26 20:45:12 -0600

    updating python file with prompt reading code

--- p1.py
+++ p1.py
@@ -5,3 +5,14 @@
 #transformers apparently provides pretrained models
 #torch lets you run the models.
 #Going to be using GPT-2 and Bert as my LLMs.
+#Finished creating repository and pushing this file to it.
+#added code to read from prompt text file
+#modified the prompt txt to not have new lines between codes.
+
+file = open("AIprompt.txt")
+prompt1 = file.readline()
+print(prompt1)
+prompt2 = file.readline()
+print(prompt2)
+prompt3 = file.readline()
+print(prompt3)
