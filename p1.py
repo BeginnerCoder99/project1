fe175494d7e52e05fcd32bd4a172fe7172d8ac78
anthony tony <anthony.ellison120@yahoo.com>
2025-02-26 20:32:37 -0600

    adding a python file

---
+++ p1.py
@@ -0,0 +1,7 @@
+#Created environment project1 in conda
+#googled steps to install LaMini-GPT
+
+#installing transformers library from hugging face using pip install transforms torch.
+#transformers apparently provides pretrained models
+#torch lets you run the models.
+#Going to be using GPT-2 and Bert as my LLMs.
