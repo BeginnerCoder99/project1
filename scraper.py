4a37e3f9f146a222f4d23938a0dc3d9700a856df
anthony tony <anthony.ellison120@yahoo.com>
2025-05-03 22:06:18 -0500

    Updating scraper file

--- scraper.py
+++ scraper.py
@@ -16,6 +16,11 @@ class Scraper(BaseComponent):
         self.max_headings = max_headings
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--user-data-dir=/tmp')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_argument('--ignore-certificate-errors')
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
